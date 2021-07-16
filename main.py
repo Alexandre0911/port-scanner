8d2bbcc3a80e4bc2aece930e7c0d4dcd65ff8700
Alexandre <71990615+Alexandre0911@users.noreply.github.com>
2021-07-16 01:09:44 +0100

    Add files via upload
    Added UDP Scan Option

--- main.py
+++ main.py
@@ -15,32 +15,51 @@ open_ports = []
 
 
 
-print('''\nScan Types:
+print('''\nPort Scan Ranges:
+[1] TCP Scan        ->      Scan Ports Opened on TCP
+[2] UDP Scan        ->      Scan Ports Opened on UDP''')
+
+ScanType = int(input('\nScan Type (1 ; 2): '))
+
+
+
+print('''\nPort Scan Ranges:
 [1] Simple Scan     ->      Port 1 to Port 1024
 [2] Advanced Scan   ->      Port 1 to Port 65536
 [3] Custom Scan     ->      Custom Port Range''')
 
-ScanType = int(input('\nScan Type (1 ; 2 ; 3): '))
+PortScanRanges = int(input('\nPort Scan Ranges (1 ; 2 ; 3): '))
+
 portnumber = 0
-if ScanType == 1:
+
+if PortScanRanges == 1:
     portnumber = 1024
     port_list = range(1, portnumber)
-elif ScanType == 2:
+elif PortScanRanges == 2:
     portnumber = 65536
     port_list = range(1, portnumber)
-elif ScanType == 3:
+elif PortScanRanges == 3:
     min_port = int(input('Enter Minimum Port >>> '))
     max_port = int(input('Enter Maximum Port >>> '))
     port_list = range(min_port, max_port+1)
 
 
+
 def portscan(port):
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((target, port))
-        return True
-    except:
-        return False
+    if ScanType == 1:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((target, port))
+            return True
+        except:
+            return False
+    elif ScanType == 2:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.connect((target, port))
+            return True
+        except:
+            return False
 
 def fill_queue(port_list):
     for port in port_list:
@@ -82,9 +101,17 @@ print('\nOpen ports are: \033[34m{}\033[m'.format(open_ports))
 
 
 
-with open('ports.txt', 'w+') as file:
-    if target not in file.read():
-        file.write('Ports Opened in {} >>>>> {}'.format(target, open_ports))
-    else:
-        print('The IP Address \033[34m{}\033[m was already scanned.')
-    file.close()
+if ScanType == 1:
+    with open('TCP_Ports.txt', 'w+') as file:
+        if target not in file.read():
+            file.write('Ports Opened in {} >>>>> {}'.format(target, open_ports))
+        else:
+            print('The IP Address \033[34m{}\033[m was already scanned.')
+        file.close()
+elif ScanType == 2:
+    with open('UDP_Ports.txt', 'w+') as file:
+        if target not in file.read():
+            file.write('Ports Opened in {} >>>>> {}'.format(target, open_ports))
+        else:
+            print('The IP Address \033[34m{}\033[m was already scanned.')
+        file.close()
